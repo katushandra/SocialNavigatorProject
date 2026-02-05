1be19aeb092da2d90c34c31f43e4291f4846b1ca
katushandra <katerina_velikaya585@mail.ru>
2026-02-05 12:10:38 +0300

    Добавление LocalDbContextFactory

---
+++ Infrastructure/Persistence/LocalDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence
+{
+    public class LocalDbContextFactory: IDesignTimeDbContextFactory<LocalDbContext>
+    {
+        public LocalDbContext CreateDbContext(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<LocalDbContext>();
+            optionsBuilder
+                .UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+
+            return new LocalDbContext(optionsBuilder.Options);
+        }
+    }
+}
